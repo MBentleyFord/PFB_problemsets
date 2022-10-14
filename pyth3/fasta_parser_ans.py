b4bcf62571a6e206755040a50bc9374ebc70bdf6
student <student@info-20.cshl.edu>
2022-10-14 17:08:46 -0400

    adding fasta_parser_ans.py

---
+++ pyth3/fasta_parser_ans.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import re
+
+fastafile=open('/users/student/prbproblemsets/pyth3/txt_files/Python_07.fasta','r')
+
+fasta_dict={}
+sequence=''
+for line in fastafile:
+    line=line.rstrip()
+    match=re.match(r'>(\S+)\s+(.*)',line) 
+    if match:
+         if sequence:
+             fasta_dict[fasta_key]=sequence
+             fasta_key=match.group(1)
+             sequenc=''
+         else:
+             fasta_key=match.group(1)
+             sequence=''
+    else:
+          sequence += line
+fasta_dict[fasta_key]=sequence
+
+for fasta_key in fasta_dict:
+   print(fasta_key,fasta_dict[fasta_key], sep='\t')
+
