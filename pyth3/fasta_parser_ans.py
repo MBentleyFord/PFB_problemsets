bfb30b52836c0e5af7496a9585cc182711a198fb
student <student@info-20.cshl.edu>
2022-10-14 17:19:45 -0400

    adding fasta_parser_ans.py with comment

--- pyth3/fasta_parser_ans.py
+++ pyth3/fasta_parser_ans.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#Create a FASTA parser, or modify your FASTA parser from the previous problem set, to use regular expressions. Also make sure your parser can deal with a sequence that is split over many lines.
 
 import re
 
